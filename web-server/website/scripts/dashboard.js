c8de1b4a335fe16f9048c1e8d0a3e022d6c317b6
alonsta <130247709+alonsta@users.noreply.github.com>
2024-11-13 17:29:06 +0200

    Delete web-server directory

--- web-server/website/scripts/dashboard.js
+++
@@ -1,9 +0,0 @@
-function switchTab(tabName) {
-    const sections = document.querySelectorAll(".content-section");
-    sections.forEach(section => section.classList.add("hidden"));
-
-    const activeSection = document.getElementById(tabName);
-    if (activeSection) {
-        activeSection.classList.remove("hidden");
-    }
-}
